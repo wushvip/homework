2ccc49075b8bdb9b50e508c4185f6907dc794fd8
wushuai_ext <wushuai_ext@cop.cmsoft.com.cn>
2021-05-14 09:27:38 +0800

    springCloud

---
+++ springCloud/bc-bdoc-config-center/config-client/src/main/java/com/chinamobile/cmss/bdoc/configclient/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.chinamobile.cmss.bdoc.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
---
+++ springCloud/bc-bdoc-config-center/config-client/src/test/java/com/chinamobile/cmss/bdoc/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinamobile.cmss.bdoc.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
